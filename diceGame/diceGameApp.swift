1a540bad20b27db92262434ff5c4cb5b8303286f
taokyone <antonsson.andreas@hotmail.com>
2023-10-02 14:18:04 +0200

    Initial Commit

---
+++ diceGame/diceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  diceGameApp.swift
+//  diceGame
+//
+//  Created by Andreas Antonsson on 2023-10-02.
+//
+
+import SwiftUI
+
+@main
+struct diceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
